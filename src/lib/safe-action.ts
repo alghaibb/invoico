5fb5f5589e512f2c8ff7c3561a4f0efa9693ace6
Moudi <mjaderi97@gmail.com>
2024-09-17 12:33:40 +1000

    Refactor handleReturnedServerError to handleServerError in safe-action.ts

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,7 +1,7 @@
 import { createSafeActionClient } from "next-safe-action"
 
 export const actionClient = createSafeActionClient({
-  handleReturnedServerError(e) {
+  handleServerError(e) {
     return "Oh no, something went wrong!";
   },
 })
