2c5b4610db5142eb06670ae19bd5e4de91688276
Moudi <mjaderi97@gmail.com>
2024-09-17 11:37:06 +1000

    Add a hover tooltip to logout button

--- src/components/navbar-buttons.tsx
+++ src/components/navbar-buttons.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LogoutButton } from "./logout-button";
+import { LogOutIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./ui/tooltip";
 
 interface NavbarButtonsProps {
   isAuthenticated: boolean; // True if user is logged in
@@ -8,13 +15,26 @@ interface NavbarButtonsProps {
 
 const NavbarButtons: React.FC<NavbarButtonsProps> = ({ isAuthenticated }) => {
   return (
-    <div className="flex space-x-4">
+    <div className="flex space-x-4 items-center justify-center">
       {isAuthenticated ? (
         <>
           <Link href="/account">
             <Button>Account</Button>
           </Link>
-          <LogoutButton>Logout</LogoutButton>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <span>
+                  <LogoutButton>
+                    <LogOutIcon />
+                  </LogoutButton>
+                </span>
+              </TooltipTrigger>
+              <TooltipContent side="top" align="end">
+                Logout
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </>
       ) : (
         <>
