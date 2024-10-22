9b21333d48e8a984d6b6a10e9684e350f728773f
Moudi <mjaderi97@gmail.com>
2024-10-22 21:48:31 +1100

    Refactor hero component to improve loading experience and add conditional rendering for CTA and message

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,13 +3,30 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaArrowDown } from "react-icons/fa";
 
 import { Button } from "@/components/ui/button";
 
+import { LoadingDots } from "./loading";
+
 // Add a prop to indicate if the user has invoices
 const Hero = ({ hasInvoice }: { hasInvoice: boolean }) => {
+  const [loading, setLoading] = useState(false);
+  const [buttonWidth, setButtonWidth] = useState<number | null>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const CTAClick = () => {
+    setLoading(true);
+  };
+
+  // Get the button's width before loading starts
+  useEffect(() => {
+    if (buttonRef.current && !loading) {
+      setButtonWidth(buttonRef.current.offsetWidth);
+    }
+  }, [loading]);
+
   return (
     <section className="relative flex items-center justify-center min-h-screen mt-20 md:mt-0">
       <motion.div
@@ -45,17 +62,33 @@ const Hero = ({ hasInvoice }: { hasInvoice: boolean }) => {
           <Link
             href={hasInvoice ? "/invoices" : "/invoices/new-invoice"}
             className="md:max-w-1"
+            onClick={CTAClick}
           >
             <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.3, duration: 0.4, ease: "easeOut" }} // Shorter duration and smaller delay
             >
-              <Button variant="hero" size="xl" className="w-full md:w-auto">
-                {hasInvoice
-                  ? "View Your Invoices"
-                  : "Create Your First Invoice"}{" "}
-                <FaArrowDown className="ml-2" />
+              <Button
+                variant="hero"
+                size="xl"
+                ref={buttonRef}
+                className="w-full md:w-auto"
+                style={{
+                  width: loading && buttonWidth ? `${buttonWidth}px` : "auto",
+                }}
+                disabled={loading}
+              >
+                {loading ? (
+                  <LoadingDots />
+                ) : (
+                  <>
+                    {hasInvoice
+                      ? "View Your Invoices"
+                      : "Create Your First Invoice"}{" "}
+                    <FaArrowDown className="ml-2" />
+                  </>
+                )}
               </Button>
             </motion.div>
           </Link>
